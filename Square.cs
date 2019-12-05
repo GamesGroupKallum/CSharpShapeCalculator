67d7cf2fb2946c4011491e79ec25939278ccd0f1
Scott-Scott-equals-new-Scott <58554273+Scott-Scott-equals-new-Scott@users.noreply.github.com>
2019-12-05 14:48:55 +0000

    Fixed InputOutput error

--- Square.cs
+++ Square.cs
@@ -12,10 +12,10 @@ namespace CSharpShapeCalculator
             length = InputOutput.CollectDouble("length");
 
             CalculateArea();
-            InputOutput.OutputResult(result, "area");
+            InputOutput.OutputResult("area", result);
 
             CalculatePerimeter();
-            InputOutput.OutputResult(result, "perimeter");
+            InputOutput.OutputResult("perimeter", result);
         }
 
         private void CalculateArea()
